d45e08f30deed5ad2620db9601d42f35fe128a4b
John Doe <cuonga2242002@gmail.com>
2024-12-01 16:12:33 +0700

    product update

--- src/routers/ProductRouter.js
+++ src/routers/ProductRouter.js
@@ -6,8 +6,9 @@ const uploadImage = require('../middleware/uploadImageMiddleware');
 const router = express.Router();
 
 router.post('/create', authMiddleware, uploadImage.single('imageUrl'), productController.createProduct);
-router.put('/update/:id', productController.updateProduct);
+router.put('/update/:id', uploadImage.single('imageUrl'), productController.updateProduct);
 router.get('/get-details/:id', productController.getDetailsProduct);
 router.delete('/delete/:id', productController.deleteProduct);
 router.get('/getAll', productController.getAllProduct);
+router.get('/getAllProduct', productController.getAllProductHomePage);
 module.exports = router;
